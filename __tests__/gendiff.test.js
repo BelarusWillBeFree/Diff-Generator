a11d09cc232eeb1b550f3e5e85ab7a15c7d86f35
Anatoli <javamuzman@gmail.com>
2021-10-31 16:01:35 +0300

    move file to dir fixture

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,24 +2,18 @@ import gendiff from '../bin/gendiff.js'
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import path from 'path';
+import { readFileSync } from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+
 test('gendiff', () => {
-   const positiveResult = `{
-- follow: false
-  host: hexlet.io
-- proxy: 123.234.53.22
-- timeout: 50
-+ timeout: 20
-+ verbose: true
-}`;
-  const fixPath1 = getFixturePath('file1.json');
-  const fixPath2 = getFixturePath('file2.json');
-   expect(gendiff(fixPath1, fixPath2)).toEqual(positiveResult);
- //   expect(reverse('')).toEqual('');
+  const sourcePath1 = getFixturePath('file1.json');
+  const sourcePath2 = getFixturePath('file2.json');
+  const fixResultFile = readFile('fixturefile1.json');
+   expect(gendiff(sourcePath1, sourcePath2)).toEqual(fixResultFile);
   });
-  //gendiff('./file1.json', './file2.json');

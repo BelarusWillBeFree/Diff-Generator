7a60d33c61eb6a028ee2b382515d5d0fc2c2bdeb
Anatoli <javamuzman@gmail.com>
2021-10-31 10:21:25 +0300

    add tests, workflow test

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,14 @@
+import gendiff from '../bin/gendiff.js'
+
+test('gendiff', () => {
+   const positiveResult = `{
+- follow: false
+  host: hexlet.io
+- proxy: 123.234.53.22
+- timeout: 50
++ timeout: 20
++ verbose: true
+}`;
+   expect(gendiff('/home/anatoli/js/frontend-project-lvl2/file1.json', './file2.json')).toEqual(positiveResult);
+ //   expect(reverse('')).toEqual('');
+  });

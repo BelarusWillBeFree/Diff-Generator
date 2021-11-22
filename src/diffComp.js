883391f91ce1adcd9f53858c8829f5074830f051
Anatoli Muzika <javamuzman@gmail.com>
2021-11-22 18:35:16 +0300

    refaktoring text

--- src/diffComp.js
+++ src/diffComp.js
@@ -6,36 +6,36 @@ const addObj = (inpSign, inpKey, inpValue) => ({ sign: inpSign, key: inpKey, val
 
 const isObjectEmpty = (objectForCheck, defaultSign = '') => (Object.keys(objectForCheck).length === 0 ? '' : defaultSign);
 
-const addDiffFromTwoObj = (key, value1, value2) => {
-  if (isObject(value1) && isObject(value2)) {
-    return addObj('=', key, makeComplexDiff(value1, value2));
-  }
-  if (!isObject(value1) && !isObject(value2)) {
-    if (value1 !== value2) {
-      return [addObj('-', key, value1), addObj('+', key, value2)];
-    }
-    return addObj('=', key, value1);
-  }
-  if (isObject(value1) !== isObject(value2)) {
-    return [addObj('-', key, makeComplexDiff(value1)), addObj('+', key, makeComplexDiff(value2))];
-  }
-};
+const getUnionSorted = (oneObject, twoObject) =>{
+  const keysFromFirstObject = Object.entries(oneObject).map(([key]) => (key));
+  const keysFromSecondObject = Object.entries(twoObject).map(([key]) => (key));
+  const unionKeys = _.union(keysFromFirstObject, keysFromSecondObject);
+  return _.sortBy(unionKeys);
 
+};
 const makeComplexDiff = (oneObject, twoObject = {}) => {
   if (!isObject(oneObject)) {
     return oneObject;
   }
-  const keysFromFirstObject = Object.entries(oneObject).map(([key]) => (key));
-  const keysFromSecondObject = Object.entries(twoObject).map(([key]) => (key));
-  const unionKeys = _.union(keysFromFirstObject, keysFromSecondObject);
-  const fullSortedKeys = _.sortBy(unionKeys);
+  const fullSortedKeys = getUnionSorted(oneObject, twoObject);
   return fullSortedKeys.flatMap((key) => {
     const value1 = oneObject[key];
     const value2 = twoObject[key];
     const obj1HasKeyProperty = Object.prototype.hasOwnProperty.call(oneObject, key);
     const obj2HasKeyProperty = Object.prototype.hasOwnProperty.call(twoObject, key);
     if (obj1HasKeyProperty && obj2HasKeyProperty) {
-        return addDiffFromTwoObj(key, value1, value2);
+      if (isObject(value1) && isObject(value2)) {
+        return addObj('=', key, makeComplexDiff(value1, value2));
+      }
+      if (!isObject(value1) && !isObject(value2)) {
+        if (value1 !== value2) {
+          return [addObj('-', key, value1), addObj('+', key, value2)];
+        }
+        return addObj('=', key, value1);
+      }
+      if (isObject(value1) !== isObject(value2)) {
+        return [addObj('-', key, makeComplexDiff(value1)), addObj('+', key, makeComplexDiff(value2))];
+      }
     }
     if (obj1HasKeyProperty) {
       return addObj(isObjectEmpty(twoObject, '-'), key, makeComplexDiff(value1));
